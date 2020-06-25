9611101e68b419e4d101eb09267403b61343171d
uttk <uttk8128@gmail.com>
2020-06-25 03:34:09 +0000

    Add page rules to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { NueNotUseHooks } from "./rules/nue-not-use-hooks";
 import { OtemElementSize } from "./rules/otem-element-size";
 import { OtemNotUseHooks } from "./rules/otem-not-use-hooks";
 import { PafeConditions } from "./rules/pafe-conditions";
+import { PageMustHaveMainTag } from "./rules/page-must-have-main-tag";
 
 export = {
   rules: {
@@ -11,5 +12,6 @@ export = {
     "otem-element-size": OtemElementSize,
     "otem-not-use-hooks": OtemNotUseHooks,
     "pafe-conditions": PafeConditions,
+    "page-must-have-main-tag": PageMustHaveMainTag,
   },
 };
